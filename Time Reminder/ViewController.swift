ec729ee080ce3c72b609be19d5210e834c9fb039
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2021-01-30 20:25:51 +0100

    Initial Commit

---
+++ Time Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Time Reminder
+//
+//  Created by Maxence Gama on 30/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
